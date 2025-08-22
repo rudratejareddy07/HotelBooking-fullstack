f794ed8a37af02023f4fd3f69d011b3594538932
rudratejareddy07 <rudratejareddy07@gmail.com>
2025-08-22 06:51:25 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './components/NavBar.jsx'
+const App=()=>{
+  return(
+    <div>
+      <NavBar/>
+      <h1>hello world</h1>
+    </div>
+  )
+}
+export default App;
