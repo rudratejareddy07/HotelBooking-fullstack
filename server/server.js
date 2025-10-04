4939144171e3b585c59677628c0b8a2fa883aed9
rudratejareddy07 <rudratejareddy07@gmail.com>
2025-10-04 17:08:02 +0530

    Add Clerk webhook handler

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
-import clerkWebhooks from "./controllers/clerkWebHooks.js";
+import clerkWebHooks from "./controllers/clerkWebHooks.js";
 
 connectDB();
 
@@ -17,7 +17,7 @@ app.use(clerkMiddleware())
 
 
 //API to listen to clerk webhooks
-app.use("/api/clerk",clerkWebhooks)
+app.use("/api/clerk",clerkWebHooks)
 
 app.get('/',(req,res)=>
     res.send("API is working fine")
